689a5a63b97dff68bfa28c3679c08d87c5b0ce1e
FL Khoa Phan <phannguyendangkhoa1995@gmail.com>
2021-05-09 23:30:19 +0700

    Zalo View

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Zalo from './src/components/Zalo'
+
+export default class App extends Component {
+  render() {
+    return (
+      <Zalo/>
+    )
+  }
+}
